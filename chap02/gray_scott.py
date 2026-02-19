80f1c044a252a2f676f557966d1d3c02abf9896b
Haruka Kubota <harukakubota.bio@gmail.com>
2026-02-19 13:37:56 +0900

    Gray-Scottモデルのノイズのサイズを変更

--- chap02/gray_scott.py
+++ chap02/gray_scott.py
@@ -1,5 +1,6 @@
 import sys, os
 sys.path.append(os.pardir)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 from alifebook_lib.visualizers import MatrixVisualizer
 
@@ -16,7 +17,7 @@ Dv = 1e-5
 f, k = 0.022, 0.051 #stripe
 #f, k = 0.04, 0.06 # amorphous
 #f, k = 0.035, 0.065 # spots
-#f, K = 0.012, 0.05 # wandering bubbles
+#f, k = 0.012, 0.05 # wandering bubbles
 #f, k = 0.025, 0.05 # waves
 
 #初期化
@@ -24,18 +25,16 @@ u = np.ones((SPACE_GRID_SIZE, SPACE_GRID_SIZE))
 v = np.zeros((SPACE_GRID_SIZE, SPACE_GRID_SIZE))
 #中央に初期パターンとしてu=0.5,v=0.25のSQUARE_SIZE四方の正方形の領域を作る
 SQUARE_SIZE = 20 
-u[SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE,
-  SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE] = 0.5
-v[SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE,
-  SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE] = 0.25
+u[SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE//2,
+  SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE//2] = 0.5
+v[SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE//2,
+  SPACE_GRID_SIZE//2-SQUARE_SIZE//2:SPACE_GRID_SIZE//2+SQUARE_SIZE//2] = 0.25
 
 #対象性を崩すために、少しノイズを入れる(ただし振れ幅が0~1だと大きすぎるので0.1をかける)
-u += np.random.rand(SPACE_GRID_SIZE, SPACE_GRID_SIZE)*0.1
-v += np.random.rand(SPACE_GRID_SIZE, SPACE_GRID_SIZE)*0.1
+u += np.random.rand(SPACE_GRID_SIZE, SPACE_GRID_SIZE)*0.05
+v += np.random.rand(SPACE_GRID_SIZE, SPACE_GRID_SIZE)*0.05
 
 while visualizer:
-      visualizer.update(u) #行列uだけを使用し絵を描く
-
       for i in range(VISUALIZATION_STEP):
             #ラプラシアンの計算
             laplacian_u = (np.roll(u, 1, axis=0) + np.roll(u, -1, axis=0) +
@@ -49,3 +48,4 @@ while visualizer:
             v += dt*dvdt
             #表示をアップデート
       visualizer.update(u)
+
